8a7b100a19cb8f94ed3595b5ecb33f76205f4ab9
Radu0924 <129392220+Radu0924@users.noreply.github.com>
2023-05-19 11:14:17 +0300

    Add files via upload

---
+++ Shark.py
@@ -0,0 +1,38 @@
+import pyshark
+import signal
+
+stop_capturing = False
+
+def packet_callback(pkt):
+    with open('Pachete.txt', 'a') as f:
+        f.write(str(pkt) + '\n')
+
+capture = pyshark.LiveCapture(interface=None)
+
+def stop_capture(signum, frame):
+    global stop_capturing
+    stop_capturing = True
+
+signal.signal(signal.SIGINT, stop_capture)  # Setează semnalul SIGINT pentru a opri capturarea (Ctrl+C)
+
+try:
+    for packet in capture.sniff_continuously():
+        packet_callback(packet)
+        print(str(packet))  # Afișează informațiile pachetului în timp real
+
+        if stop_capturing:
+            break
+except KeyboardInterrupt:
+    pass
+
+with open('Pachete.txt', 'r') as f:
+    while True:
+        line = f.readline()
+        if line:
+            print(line.strip())  # Afișează informațiile din fișier în timp real
+        else:
+            break
+#Bibliografie
+#https://machineperson.github.io/assets/traffic%20analysis.pdf
+#https://www.tutorialspoint.com/python_penetration_testing/python_penetration_testing_network_packet_sniffing.htm
+# + instalare pyshark
